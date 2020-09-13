9a2429f47179c7c578c587538597e823b19db758
Tiago José <tiago_jvo@outlook.com>
2020-09-13 17:40:08 -0300

    feat: ensure Login present valid state  if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -21,11 +21,11 @@ const Input: React.FC<Props> = (props: Props) => {
   }
 
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🔵'
   }
 
   const getTitle = (): string => {
-    return error
+    return error || 'Tudo Certo'
   }
 
   return (
